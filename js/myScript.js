a6177f1eb88dde9ea9fe437627a8242b6bac51b9
ppedrovit01r <ale21002@byui.edu>
2021-02-03 18:45:50 -0300

    w5

--- js/myScript.js
+++ js/myScript.js
@@ -7,3 +7,17 @@ function addNumbers() {
     //OUTPUT
     document.getElementById('output').innerHTML = "The sum of " + x + " and " + y + " equals " + sum + "!";
 }
+
+function convertPoundsToKilograms() {
+    //INPUT - Receive the number in Pounds.
+    let lbs = parseFloat(document.getElementById('pounds').value);
+    //PROCESSING Pt.1 - 1 lbs equals 0.45359237 kg, so we should let the computer know that:
+    let kg = 0.45359237 * lbs;
+    //PROCESSING Pt.2 - Then, round the number to one digit after the decimal:
+    let digits = 1;
+    let multiplier = Math.pow(10, digits);
+    lbs = Math.round(lbs * multiplier) / multiplier;
+    kg = Math.round(kg * multiplier) / multiplier;
+    //OUTPUT - Display the result found through processing.
+    document.getElementById('output').innerHTML = lbs + " pounds equals " + kg + " kilograms."
+}

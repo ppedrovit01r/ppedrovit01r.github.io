121b9470102b62c6cf53da151fc7e672b98ce4ea
ppedrovit01r <ale21002@byui.edu>
2021-02-25 21:12:33 -0300

    w8.2

--- js/myScript.js
+++ js/myScript.js
@@ -161,3 +161,46 @@ function sumOdds() {
   document.getElementById("output").innerHTML =
     "Sum of Odd Values: " + sum;
 }
+
+function loop1() {
+  let output1 = "";
+  let i = 0;
+  while (i < 2) { //should be smaller than 2
+      output1 += `Part 1:${i}<br>`;
+      i++;
+  }
+  document.getElementById('display1').innerHTML = output1;
+}
+
+function loop2() {
+  var output2 = "";
+  for (let j = 0; j < 3; j++) {
+    if (j != 2) { //Only one equal sign
+      output2 += `Part 2:${j}, `;
+    }
+    else {
+      output2 += `Part 2:${j}`;
+    }  
+  }
+  document.getElementById('display2').textContent = output2;
+}
+
+function loop3() {
+  let output3 = "";
+  for (let k = 0; k < 4; k++) { //Semicolons, not commas
+    output3 += k + " ";
+  }
+  document.getElementById('display3').textContent = output3;
+}
+
+function loop4() {
+  let balance = 1000;
+  const rate = .10;
+  let n = 30;
+  for (let s = 1; s < n; s++){ //changed S to start on 1
+      interest = balance * rate;
+      balance += interest;
+  }
+  balance = Math.round(balance*100)/100 //rounding number
+  document.getElementById('display4').textContent = balance;
+}

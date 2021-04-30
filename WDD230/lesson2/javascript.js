d1769ee92d09b042787ee106b88920ebbc970d27
ppedrovit01r <ale21002@byui.edu>
2021-04-30 11:53:34 -0300

    w2..

---
+++ WDD230/lesson2/javascript.js
@@ -0,0 +1,7 @@
+//footer details
+      document.getElementById(
+        "currentYear"
+      ).innerHTML = new Date().getFullYear();
+      document.getElementById("lastModified").innerHTML = new Date(
+        document.lastModified
+      );

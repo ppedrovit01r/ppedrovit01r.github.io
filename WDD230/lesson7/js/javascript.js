a966a5cef9e34e7eae3beb3ab656d75002509a55
ppedrovit01r <ale21002@byui.edu>
2021-06-04 15:11:31 -0300

    w7.3

---
+++ WDD230/lesson7/js/javascript.js
@@ -0,0 +1,28 @@
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+//footer details
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+//current date
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.textContent = fulldate;
+
+//display Pancake at the park only on Fridays
+if (now.getDay() == 5) {
+  document.querySelector('.banner').style.display = "block"
+}

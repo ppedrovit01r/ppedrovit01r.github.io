a4028f26d413135feadd9ffc148647c8dd5a75b0
ppedrovit01r <ale21002@byui.edu>
2021-06-16 15:22:30 -0300

    w9.1

---
+++ WDD230/lesson9/js/javascript.js
@@ -0,0 +1,49 @@
+// ********************** NAVIGATION ****************/
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+};
+
+//footer details
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+//current date
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.textContent = fulldate;
+
+//display Pancake at the park only on Fridays
+//if (now.getDay() == 5) {
+//  document.querySelector('.banner').style.display = "block"
+//}
+
+//************************** STORAGE *****************/
+let daysSinceLastVisit;
+
+if (!localStorage.getItem('lastVisit')) {
+  localStorage.setItem('lastVisit', now.getTime());
+  document.querySelector('.visitedGallery').innerHTML = "Thank you for visiting our page! 🙏"
+} else {
+  let miliseconds = now.getTime() - localStorage.getItem('lastVisit');
+  let oneDay = 1000 * 60 * 60 * 24;
+  daysSinceLastVisit = Math.round(miliseconds/oneDay);
+}
+
+//document.getElementById("visits").innerHTML = daysSinceLastVisit;
+
+/************************ FORM  *******************/
+function adjustRange(range) {
+  document.getElementById("rangeValue").innerHTML = range;
+}

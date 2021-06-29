b9d2427292dc9130a39bd23bf1178191020cc8e2
ppedrovit01r <ale21002@byui.edu>
2021-06-29 12:45:53 -0300

    w11.0

---
+++ WDD230/lesson11/js/weatherapi.js
@@ -0,0 +1,90 @@
+const PrestonId = "5604473";
+const myId = "f6b72466de41a9b962d33a29f3f1e123";
+
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?id=${PrestonId}&appid=${myId}&units=imperial`;
+fetch(weatherURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    let weather = jsObject;
+
+    let t = weather.main.temp;
+    let s = weather.wind.speed;
+    const windChill = document.querySelector("#windChill");
+    if (t <= 50 && s > 3) {
+      output =
+        35.74 +
+        0.6215 * t -
+        35.75 * Math.pow(s, 0.16) +
+        0.4275 * t * Math.pow(s, 0.16);
+      output = Math.round(output) + "&deg;F";
+    } else {
+      output = `N/A`;
+    }
+    //output
+    windChill.innerHTML = output;
+    document.querySelector("#currentTemp").innerHTML = `${t.toFixed(0)}&deg;F`;
+    document.querySelector("#currentDesc").innerHTML = weather.weather[0].main;
+    document.querySelector("#humidity").innerHTML = `${weather.main.humidity}%`;
+    document.querySelector("#windSpeed").innerHTML = `${s.toFixed(1)} mph`;
+
+    const imagesrc = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+    document.getElementById("iconTemp").setAttribute("src", imagesrc);
+    document
+      .getElementById("iconTemp")
+      .setAttribute("alt", weather.weather[0].main);
+  });
+
+//get day of the week
+let dayOfWeek = new Date().getDay();
+
+function getWeekDay(dayOfWeek) {
+  let weekdays = new Array(
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu"
+  );
+  return weekdays[dayOfWeek];
+}
+
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=${PrestonId}&appid=${myId}&units=imperial`;
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    let forecast = jsObject;
+
+    let ul = document.createElement("ul");
+
+    let forecast18pm = forecast.list.filter(forecast => forecast.dt_txt.includes('18:00:00'));
+    //console.log(forecast18pm);
+
+    for (let i = 0; i < 5; i++) {
+      let li = document.createElement("li");
+      let day = document.createElement("h3");
+      let icon = document.createElement("img");
+      let desc = document.createElement("p");
+
+      day.innerHTML = getWeekDay(1 + dayOfWeek + i);
+      icon.setAttribute('src', `https://openweathermap.org/img/wn/${forecast18pm[i].weather[0].icon}@2x.png`);
+      icon.setAttribute('alt', forecast18pm[i].weather[0].main)
+      desc.innerHTML = `${
+        forecast18pm[i].weather[0].main
+      } <strong>${forecast18pm[i].main.temp.toFixed(0)}&deg;F</strong>`;
+
+      li.appendChild(day);
+      li.appendChild(icon);
+      li.appendChild(desc);
+      ul.appendChild(li);
+    }
+    document.querySelector("div.forecast").appendChild(ul);
+  });

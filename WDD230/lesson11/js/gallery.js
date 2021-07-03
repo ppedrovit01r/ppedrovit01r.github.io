44785b650a3c85bbd63951c4ff1d90465d24f40f
ppedrovit01r <ale21002@byui.edu>
2021-07-03 16:05:10 -0300

    t

--- WDD230/lesson11/js/gallery.js
+++ WDD230/lesson11/js/gallery.js
@@ -8,6 +8,7 @@ if (!localStorage.getItem('lastVisit')) {
   let miliseconds = now.getTime() - localStorage.getItem('lastVisit');
   let oneDay = 1000 * 60 * 60 * 24;
   daysSinceLastVisit = Math.round(miliseconds/oneDay);
+  localStorage.setItem('lastVisit', now.getTime());
 }
 
 document.getElementById("visits").innerHTML = daysSinceLastVisit;

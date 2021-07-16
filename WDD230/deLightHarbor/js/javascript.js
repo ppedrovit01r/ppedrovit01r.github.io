d19626190473e1d90561715efd863fb6fc335322
ppedrovit01r <ale21002@byui.edu>
2021-07-16 16:39:08 -0300

    t

---
+++ WDD230/deLightHarbor/js/javascript.js
@@ -0,0 +1,5 @@
+/***************** HAMBURGER ICON *********************/
+function toggleMenu(x) {
+    x.classList.toggle("change");
+    document.querySelector("header").classList.toggle("responsive");
+}

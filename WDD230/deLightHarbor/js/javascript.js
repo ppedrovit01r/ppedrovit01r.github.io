61c5270839d9f526d518c7ceeb627b55ed42be2f
ppedrovit01r <ale21002@byui.edu>
2021-07-19 20:57:54 -0300

    t

--- WDD230/deLightHarbor/js/javascript.js
+++ WDD230/deLightHarbor/js/javascript.js
@@ -1,5 +1,32 @@
-/***************** HAMBURGER ICON *********************/
-function toggleMenu(x) {
+/***************** FUNCTIONS *********************/
+function toggleMenu(x) /* Toggles the Navigation bar */ {
     x.classList.toggle("change");
     document.querySelector("header").classList.toggle("responsive");
 }
+
+function DateBrazil() /* Gets the time in Porto Alegre, Brazil */{
+    let d = new Date();
+    if (d.getTimezoneOffset() != 180) {
+        utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+        let nd = new Date(utc + (3600000*(-3))); // (-3) is the time in Brazil
+        return nd; // return time as a string
+    } else {return d;}
+}
+
+function checkHQStatus() /* Displays if we're inside working hours (7AM-7PM) */ {
+    let d = DateBrazil();
+    if (d.getDay() != 0 && d.getHours()>=7 && d.getHours()<19) {
+        document.querySelector("#hqStatus").textContent = "Open";
+        document.querySelector("#hqStatus").classList.add("addGreen");
+    } else {
+        document.querySelector("#hqStatus").textContent = "Closed";
+        document.querySelector("#hqStatus").classList.add("addRed");
+    }
+}
+
+/***************** CONTACT PAGE *********************/
+if (document.querySelector(".activePage").textContent == `Contact Us`) {
+    checkHQStatus();
+} /*************end of contact page query**********/
+
+console.log(DateBrazil());

6f27e37fc70a83f543fb1f6338d7a9f8df9c9f04
ppedrovit01r <ale21002@byui.edu>
2021-07-20 19:10:48 -0300

    t

--- WDD230/deLightHarbor/js/javascript.js
+++ WDD230/deLightHarbor/js/javascript.js
@@ -34,8 +34,74 @@ function directoryAdd() /* Building content from JSON data source */ {
       return response.json();
     })
     .then(function (jsonObject) {
-      console.table(jsonObject);
+      const dict = jsonObject["directory"];
+      for (let i = 0; i < dict.length; i++) {
+          let card = document.createElement('div');
+          card.classList.add("listView");
+          
+          let icon = document.createElement('img');
+          icon.setAttribute('src', 'images/' + dict[i].logo);
+          icon.setAttribute('alt', dict[i].name.toLowerCase() + " logo");
+          icon.setAttribute('loading', 'lazy');
+          card.appendChild(icon);
+
+          let h2 = document.createElement('h2');
+          h2.textContent = dict[i].name;
+          card.appendChild(h2);
+
+          let info = document.createElement('div');
+          let address = document.createElement('p');
+          address.innerHTML = `<strong>Address: </strong>${dict[i].address}`;
+          info.appendChild(address);
+
+          let div = document.createElement('div');
+          div.classList.add("telFlex")
+          let tel = document.createElement('p');
+          tel.innerHTML = `<strong>Tel: </strong>${dict[i].tel.stylized}`;
+          let fixo = document.createElement('a');
+          fixo.setAttribute('href', "tel:" + dict[i].tel.number);
+          fixo.textContent = "CALL ☎";
+          div.appendChild(tel);
+          div.appendChild(fixo);
+          if (dict[i].tel.type == "whatsapp") {
+              let whatsapp = document.createElement('a');
+              whatsapp.setAttribute('href', "https://wa.me/" + dict[i].tel.number);
+              whatsapp.setAttribute('target', "_blank");
+              whatsapp.textContent = "MESSAGE ✉";
+              div.appendChild(whatsapp);
+          }
+          info.appendChild(div);
+          card.appendChild(info);
+
+          let site = document.createElement('p');
+          site.innerHTML = `<strong><a target="_blank" href="${dict[i].link}">Check their website</a></strong>`;
+          card.appendChild(site);
+          
+          document.querySelector('div.directoryAdd').appendChild(card);
+      }
     });
+}
+
+function toggleDirectory(x) /* Toggles the Directory from List View to Grid View */ {
+    x.classList.remove("icon-unactive");
+    document.querySelector(".icon-active").setAttribute('onclick', "toggleDirectory(this)");
+    document.querySelector(".icon-active").classList.add("icon-unactive");
+    document.querySelector(".icon-active").classList.remove("icon-active");
+    x.removeAttribute('onclick', "toggleDirectory(this)");
+    x.classList.add("icon-active");
+
+    document.querySelector(".directoryAdd").classList.toggle("max3Columns");
+    if (document.querySelector(".directoryAdd").classList.length > 1) /*means it has changed to GridView*/{
+        for (let i = 0; i < 8; i++) {
+            document.querySelector(".listView").classList.add("gridView");
+            document.querySelector(".listView").classList.remove("listView");
+        }
+    } else /*it has changed to ListView*/{
+        for (let i = 0; i < 8; i++) {
+            document.querySelector(".gridView").classList.add("listView");
+            document.querySelector(".gridView").classList.remove("gridView");
+        }
+    }
 }
 
 /***************** CONTACT PAGE *********************/
@@ -43,9 +109,10 @@ if (document.querySelector(".activePage").textContent == `Contact Us`) {
   checkHQStatus();
 } /*************end of contact page query**********/
 
-/***************** city PAGE *********************/
-if (document.querySelector(".activePage").textContent == `Porto Alegre`) {
-  lightGallery(document.getElementById("lightgallery"));
+/***************** DIRECTORY PAGE *********************/
+if (document.querySelector(".activePage").textContent == `Directory`) {
+  directoryAdd();
+  console.table(document.querySelector(".directoryAdd").classList.length);
 } /*************end of contact page query**********/
 
 const dictFile1 =

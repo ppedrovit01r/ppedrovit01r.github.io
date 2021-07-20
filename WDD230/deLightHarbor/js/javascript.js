14ac8f4209a9ab40d6a0088c59ce0b17c74d6976
ppedrovit01r <ale21002@byui.edu>
2021-07-20 16:23:40 -0300

    t

--- WDD230/deLightHarbor/js/javascript.js
+++ WDD230/deLightHarbor/js/javascript.js
@@ -1,39 +1,59 @@
 /***************** FUNCTIONS *********************/
 function toggleMenu(x) /* Toggles the Navigation bar */ {
-    x.classList.toggle("change");
-    document.querySelector("header").classList.toggle("responsive");
+  x.classList.toggle("change");
+  document.querySelector("header").classList.toggle("responsive");
 }
 
-function DateBrazil() /* Gets the time in Porto Alegre, Brazil */{
-    let d = new Date();
-    if (d.getTimezoneOffset() != 180) {
-        utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-        let nd = new Date(utc + (3600000*(-3))); // (-3) is the time in Brazil
-        return nd; // return time as a string
-    } else {return d;}
+function DateBrazil() /* Gets the time in Porto Alegre, Brazil */ {
+  let d = new Date();
+  if (d.getTimezoneOffset() != 180) {
+    utc = d.getTime() + d.getTimezoneOffset() * 60000;
+    let nd = new Date(utc + 3600000 * -3); // (-3) is the time in Brazil
+    return nd; // return time as a string
+  } else {
+    return d;
+  }
 }
 
 function checkHQStatus() /* Displays if we're inside working hours (7AM-7PM) */ {
-    let d = DateBrazil();
-    if (d.getDay() != 0 && d.getHours()>=7 && d.getHours()<19) {
-        document.querySelector("#hqStatus").textContent = "Open";
-        document.querySelector("#hqStatus").classList.add("addGreen");
-    } else {
-        document.querySelector("#hqStatus").textContent = "Closed";
-        document.querySelector("#hqStatus").classList.add("addRed");
-    }
+  let d = DateBrazil();
+  if (d.getDay() != 0 && d.getHours() >= 7 && d.getHours() < 19) {
+    document.querySelector("#hqStatus").textContent = "Open";
+    document.querySelector("#hqStatus").classList.add("addGreen");
+  } else {
+    document.querySelector("#hqStatus").textContent = "Closed";
+    document.querySelector("#hqStatus").classList.add("addRed");
+  }
 }
 
-
+function directoryAdd() /* Building content from JSON data source */ {
+   const dictFile =
+    "https://ppedrovit01r.github.io/WDD230/deLightHarbor/directory.json";
+   fetch(dictFile)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+      console.table(jsonObject);
+    });
+}
 
 /***************** CONTACT PAGE *********************/
 if (document.querySelector(".activePage").textContent == `Contact Us`) {
-    checkHQStatus();
+  checkHQStatus();
 } /*************end of contact page query**********/
 
 /***************** city PAGE *********************/
 if (document.querySelector(".activePage").textContent == `Porto Alegre`) {
-    lightGallery(document.getElementById('lightgallery'));
+  lightGallery(document.getElementById("lightgallery"));
 } /*************end of contact page query**********/
 
-console.log(DateBrazil());
+const dictFile1 =
+    "https://ppedrovit01r.github.io/WDD230/deLightHarbor/directory.json";
+   fetch(dictFile1)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+      console.table(jsonObject);
+    });

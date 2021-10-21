3210ff555b11db90181a9ec1b9b43811923ed853
ppedrovit01r <ale21002@byui.edu>
2021-10-21 16:31:53 -0300

    t

--- CSE110/W07/pillowtest.py
+++ CSE110/W07/pillowtest.py
@@ -6,4 +6,22 @@ image_original = Image.open("beach.jpg")
 
 # This line attempts to open a new window to display the image.
 image_original.show()
-print("The library is loaded correctly")
+
+# This line gets the size of the image
+width, height = image_original.size
+
+# This line gets the pixels information of the image and stores them
+pixels_original = image_original.load()
+
+# This line accesses the RGB values of the coordinates (x,y) of the image
+x = 100
+y = 200
+r, g, b = pixels_original[x, y]
+print(pixels_original[x,y])
+
+# This line sets new RGB values for the pixel selected to purple (255, 0, 255)
+pixels_original[x, y] = (255, 0, 0)
+print(pixels_original[x, y])
+
+# This line saves an image
+image_original.save("the_file_goes_here.jpg")
